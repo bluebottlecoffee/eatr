be272f88acc724f485a2ddf4338c8770e322cc74
Greggory Rothmeier <greggroth@gmail.com>
2016-11-28 15:29:02 -0800

    Fix spec for recent change

--- spec/xml/schema_generator_spec.rb
+++ spec/xml/schema_generator_spec.rb
@@ -10,23 +10,31 @@ remove_namespaces: true
 fields:
 - name: author_first_name
   xpath: "/book/author/firstName"
+  type: string
 - name: author_last_name
   xpath: "/book/author/lastName"
+  type: string
 - name: publisher_name
   xpath: "/book/publisher/name"
+  type: string
 - name: published_at
   xpath: "/book/publishedAt"
+  type: string
 - name: for_sale
   xpath: "/book/forSale"
+  type: string
 - name: rating
   xpath: "/book/rating"
+  type: string
 - name: pages
   xpath: "/book/pages"
+  type: string
 - node: chapters
   xpath: "/book/chapters/chapter"
   children:
   - name: chapters_title
     xpath: "./title"
+    type: string
 EXPECTED
   end
 end
