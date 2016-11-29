bd1b814f542e7f5feff17e0d04ec2794f7b027c3
Greggory Rothmeier <greggroth@gmail.com>
2016-11-28 21:17:20 -0800

    Add _very_ simple SQL CREATE statement generator
    This should _not_ be used directly, but it's output used as a
    90%-of-the-way-there headstart.  In my case, I have over a dozen large
    schema files that need accompaning tables, so this is much easier for me
    than hand-writing them all.

---
+++ spec/eatl/sql/table_generator_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require './lib/eatl/sql/table_generator'
+
+describe Eatl::Sql::TableGenerator do
+  it 'generates a CREATE TABLE statement from a schema' do
+    generator = described_class.new('./spec/fixtures/schema/book.yaml')
+    expect(generator.statement).to eq(<<-EXPECTED)
+CREATE TABLE book (
+  author TEXT NOT NULL,
+  pages INT NOT NULL,
+  for_sale BOOLEAN NOT NULL,
+  published_at TIMESTAMP NOT NULL,
+  rating REAL NOT NULL,
+  icbn TEXT,
+  summary VARCHAR(15) NOT NULL,
+  age INT NOT NULL
+);
+EXPECTED
+  end
+end
